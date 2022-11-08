4942918c69ebdd8c016d918cdf79a4727ea94529
Gordon Ball <gball261@gmail.com>
2022-11-08 18:59:42 -0500

    Added order schema

--- routes/shipments.js
+++ routes/shipments.js
@@ -4,7 +4,11 @@ const express = require("express");
 const { BadRequestError } = require("../expressError");
 const router = new express.Router();
 
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schemas/orderSchema.json");
+
 const { shipProduct } = require("../shipItApi");
+const { json } = require("express");
 
 /** POST /ship
  *
@@ -18,8 +22,17 @@ router.post("/", async function (req, res, next) {
   if (req.body === undefined) {
     throw new BadRequestError();
   }
+
+  const result = jsonschema.validate(
+    req.body?.order, orderSchema, { required: true });
+  if (!result.valid) {
+    const errs = results.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
+
   return res.json({ shipped: shipId });
 });
 
