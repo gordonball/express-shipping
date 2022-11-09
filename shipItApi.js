126d6209483d341502c15381f899ce1678d6498c
Gordon Ball <gball261@gmail.com>
2022-11-08 21:12:23 -0500

    Mock Axios Test

--- shipItApi.js
+++ shipItApi.js
@@ -25,7 +25,7 @@ async function shipProduct({ productId, name, addr, zip }) {
       key: SHIPIT_API_KEY
     },
   });
-
+  
   return resp.data.receipt.shipId;
 }
 
--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,28 @@
 "use strict";
 
+const axios = require("axios");
+const AxiosMockAdapter = require("axios-mock-adapter");
+
+const axiosMock = new AxiosMockAdapter(axios);
+
+
 const {
   shipProduct,
 } = require("./shipItApi");
 
 
 test("shipProduct", async function () {
+
+  axiosMock.onPost("http://localhost:3001/ship")
+    .reply(200, {
+      receipt: {
+        itemId: 1001,
+        name: 'stuff',
+        addr: '123 blah street',
+        shipId: 1
+      }
+})
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +30,9 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  console.log("SHIP ID", shipId)
+
+  expect(shipId).toEqual(1);
 });
+
+
